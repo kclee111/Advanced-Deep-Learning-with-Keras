8f3f29bef89da55f5b458da01e209cbd241efe2b
kclee111 <67255391+kclee111@users.noreply.github.com>
2024-05-13 17:27:19 +0900

    Update myutils.py

--- myutils.py
+++ myutils.py
@@ -103,3 +103,9 @@ def set_font(font: str = "NanumBarunGothic"):
     matplotlib.rcParams["axes.unicode_minus"] = False
 
 set_font()
+api_key = dotdict()
+api_key.polygon = "xvPe15eQI1f0jbwJWmETWyLdZUgowTgT"
+api_key.alpha_vantage = "L9BJ9D92PQJO0R2Q"
+api_key.intrinio = "OjBlNjZkMWRhMTM0ZjM1MzI0M2RiOWIxZGNjY2Y5YWQy"
+api_key.nasdaqdatalink = "n29yrod1VqBxiC6k18e8"
+
