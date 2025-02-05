36ff2439430c211fb002cb870bde0c3f4b442c46
kclee111 <67255391+kclee111@users.noreply.github.com>
2025-02-05 10:49:03 +0900

    Update css.py

--- css.py
+++ css.py
@@ -2,16 +2,11 @@ from IPython.core.display import HTML
 HTML(r"""
 <style>
     * {
-    #     #color: red;
-    #     #font-family: ‘Cascadia Code PL’;#,‘Courier New’, Courier, monospace;
-    #     font-family: "Consolas" !important;
         font-size: 18px !important;
         line-height: 1.2 !important;
     }
     .output-plaintext, .output-stream, .output {
         font-family: "Consolas" !important; # Any monospaced font should work
-        # line-height: 1.3 !important;
-        # font-size: 12px !important;
     }
 </style>
 """)
